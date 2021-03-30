6f1ac6393d3978454b15184097cc9a38794cbf73
tore88 <sa.muredda43@gmail.com>
2021-03-30 19:48:00 +0100

    commited for Cy

--- src/Components/Newpost/Newpost.js
+++ src/Components/Newpost/Newpost.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import classes from "./Newpost.module.css";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 class Newpost extends Component {
   state = {
-    title: `0`,
-    content: `0`,
-    author: `0`,
+    post: {
+      title: `title`,
+      body: `text`,
+      author: `Author`,
+    },
   };
 
+  sendDataHadler() {
+    const data = { ...this.state.post };
+    // {
+    //   title: this.state.title,
+    //   body: this.state.body,
+    //   author: this.state.author,
+    // };
+    console.log(data);
+
+    // axios
+    //   .post("https://jsonplaceholder.typicode.com/posts", data)
+    //   .then((response) => console.log(response));
+  }
+
   render() {
+    console.log(this.state.post);
     return (
       <div className={classes.myContainer}>
         <div className="container">
@@ -45,9 +63,9 @@ class Newpost extends Component {
             <div className="col-sm-12">
               <textarea
                 rows="5"
-                value={this.state.content}
+                value={this.state.body}
                 onChange={(event) =>
-                  this.setState({ content: event.target.value })
+                  this.setState({ body: event.target.value })
                 }
                 className={classes.textarea}
               />
@@ -55,13 +73,29 @@ class Newpost extends Component {
           </div>
           <div className="row">
             <div className="col-sm-12">
-              <label>Author</label>
+              {/* <div>
+                <label>Author</label>
+
+                <select
+                  className="form-select"
+                  value={this.state.author}
+                  onChange={(event) =>
+                    this.setState({ author: event.target.value })
+                  }
+                >
+                  <option value="Author1">Author1</option>
+                  <option value="Author2">Author2</option>
+                  <option value="Author3">Author3</option>
+                </select>
+              </div> */}
             </div>
           </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <Button className={classes.button}> Add post</Button>
-            </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <Button className={classes.button} onClick={this.sendDataHadler}>
+              Add post
+            </Button>
           </div>
         </div>
       </div>
